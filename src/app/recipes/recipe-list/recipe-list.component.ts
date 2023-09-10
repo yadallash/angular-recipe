2485fdb2d2e20749674bbb81ef956908899f7032
yadallash <yaseenaldallash@gmail.com>
2023-09-09 22:26:35 -0400

    worked on recipe model, list component, and detail,

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../models/recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe('A test Recipe','This is a simple a test ', 'https://carnivalmunchies.com/wp-content/uploads/2015/09/tom-yum-1.jpg'),
+  ];
 
 }
