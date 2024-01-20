58ecf766a1fe74a878ccc462a6210ac8ac7c2d31
yadallash <yaseenaldallash@gmail.com>
2024-01-19 18:20:21 -0600

    added dropdown directive and hooked it up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-it
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { ShoppingListService } from './shopping-list/services/shopping-list.service';
+import { DropdownDirective } from './shared/dropdown.directive';
 
 @NgModule({
   declarations: [
@@ -23,6 +24,7 @@ import { ShoppingListService } from './shopping-list/services/shopping-list.serv
     ShoppingListComponent,
     ShoppingEditComponent,
     ShoppingEditComponent,
+    DropdownDirective
   ],
   imports: [
     BrowserModule,
